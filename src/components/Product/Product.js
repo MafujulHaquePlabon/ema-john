baee0eb5bd8b9b69254cbe4a2dacf7697d561a05
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-03-24 15:32:18 +0600

    Header component added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,11 +6,17 @@ const Product = (props) => {
     return (
            <div className="product">
            <img src={img} alt="" />
+            <div className="product-info">
             <h1 className="product-name">{name}</h1>
             <h4>Price:${price}</h4>
             <p><small>Manufacturer: {seller}</small></p>
             <p><small> Rating:{ratings} star</small></p>
+            </div>
+            <button className="btn-cart">
+               <p> Add to cart</p>
+                </button>
            </div>
+           
     );
 };
 
